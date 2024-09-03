19f39d50235253b6373f2bc2eef1bb39b9194e04
Nanyonga-Rahmah <nanyongarahmah891@gmail.com>
2024-09-04 00:50:10 +0300

    Eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+export default [{
+    settings: {
+        next: {
+            rootDir: true,
+        },
+    },
+
+    rules: {
+        "react/jsx-key": "off",
+        "react/no-unescaped-entities": "off",
+    },
+}];
