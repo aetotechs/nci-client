7edabe19b9f6e4bc8ede7a5e2af2706a57a637e6
Nanyonga-Rahmah <nanyongarahmah891@gmail.com>
2024-09-04 10:09:23 +0300

    build error

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
- import js from "@eslint/js";
+import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
@@ -19,6 +19,8 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
     }
   }
