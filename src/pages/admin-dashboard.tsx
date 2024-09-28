705fa651ab829672619560e1490b7e9b15f5775d
Nanyonga-Rahmah <nanyongarahmah891@gmail.com>
2024-09-28 19:54:11 +0300

    Dashboard

--- src/pages/admin-dashboard.tsx
+++ src/pages/admin-dashboard.tsx
@@ -13,25 +13,23 @@ function Admin() {
   return (
     <div className={`flex w-[100vw]  relative ${isCollapsed && ' '}`}>
       <div
-        className={` w-[15vw] hidden md:flex bg-white h-screen border-r border-primary/30 sticky top-0  transition-all duration-300 ${isCollapsed && 'w-[5vw] overflow-x-hidden'}`}
-      >
+        className={` w-[15vw] hidden md:flex bg-white h-screen border-r border-primary/30 sticky top-0  transition-all duration-300 ${isCollapsed && 'w-[5vw] overflow-x-hidden'}`}>
         <AdminSideBarDesktop isCollapsed={isCollapsed} toggleCollapse={toggleCollapse} />
       </div>
 
       <div className="absolute top-4 hidden md:flex">
         <button
           onClick={toggleCollapse}
-          className={`text-primary bg-white shadow-md  z-50 fixed  rounded-sm translate-x-[14vw] ${isCollapsed && 'translate-x-[4vw]'}`}
-        >
+          className={`text-primary bg-white shadow-md  z-50 fixed  rounded-sm translate-x-[14vw] ${isCollapsed && 'translate-x-[4vw]'}`}>
           {isCollapsed ? <ChevronRight className="h-5 w-5" /> : <ChevronLeft className=" " />}
         </button>
       </div>
       <div
-        className={`w-[100vw]   transition-all duration-100 ${isCollapsed ? 'w-[95vw]' : 'w-[85vw]'} `}
-      >
+        className={`w-[100vw]   transition-all duration-100 ${isCollapsed ? 'w-[95vw]' : 'w-[85vw]'} `}>
         <AdminHeader />
         <AdminMobileNav />
-        <div className="md:px-5">
+        <div
+          className={`px-6 py-5 overflow-hidden w-[98vw] ${isCollapsed ? 'md:w-[94vw] ' : 'md:w-[84vw]'}`}>
           <Dashboard />
         </div>
       </div>
